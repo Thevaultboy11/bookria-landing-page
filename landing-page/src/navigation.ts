9119cde3e448604072fd5322379293db845f8edb
voodoohacker <thevaultboy11@gmail.com>
2024-09-01 17:47:28 +0200

    adding the astrowind template

---
+++ landing-page/src/navigation.ts
@@ -0,0 +1,53 @@
+import { getPermalink, getBlogPermalink, getAsset } from './utils/permalinks';
+
+export const headerData = {
+  links: [
+    {
+      text: 'Banned Books',
+      links: [
+        {
+          text: 'SaaS',
+          href: getPermalink('/homes/saas'),
+        },
+        {
+          text: 'Startup',
+          href: getPermalink('/homes/startup'),
+        },
+        {
+          text: 'Mobile App',
+          href: getPermalink('/homes/mobile-app'),
+        },
+        {
+          text: 'Personal',
+          href: getPermalink('/homes/personal'),
+        },
+      ],
+    },
+  ],
+};
+
+export const footerData = {
+  links: [
+    {
+      title: 'Product',
+      links: [
+        { text: 'Features', href: '#' },
+        { text: 'Security', href: '#' },
+        { text: 'Team', href: '#' },
+        { text: 'Team', href: '#' },
+      ],
+    },
+  ],
+  secondaryLinks: [
+    { text: 'Terms', href: getPermalink('/terms') },
+    { text: 'Privacy Policy', href: getPermalink('/privacy') },
+  ],
+  socialLinks: [
+    { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: '#' },
+    { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: '#' },
+  ],
+  footNote: `
+    
+    Made by <a class="text-blue-600 underline dark:text-muted" href="https://bookria.co"> Bookria</a> · All rights reserved.
+  `,
+};
---
+++ landing-page/tailwind.config.cjs
@@ -0,0 +1,33 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import typographyPlugin from '@tailwindcss/typography';
+
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,json,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      aspectRatio: {
+        '32/50': '32 / 50', // Custom aspect ratio class
+        '4/3': '1 / 3',
+      },
+      colors: {
+        primary: '#9d4edd',
+        primary_hover: '#7F3FB3',
+        secondary: 'var(--aw-color-secondary)',
+        accent: 'var(--aw-color-accent)',
+        default: 'var(--aw-color-text-default)',
+        muted: 'var(--aw-color-text-muted)',
+        'book-yellow': '#fce62e',
+        'book-gray': '#1a1a1c',
+        'book-theme': '#9d4edd',
+        'book-hover': '#7F3FB3',
+      },
+      fontFamily: {
+        sans: ['var(--aw-font-sans, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+        serif: ['var(--aw-font-serif, ui-serif)', ...defaultTheme.fontFamily.serif],
+        heading: ['var(--aw-font-heading, ui-sans-serif)', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [typographyPlugin],
+  darkMode: 'class',
+};
